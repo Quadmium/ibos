3039ac296e46320950468c622f6d137d3f7e5d8c
quadmium <Quadmium@gmail.com>
2020-10-27 13:33:46 -0500

    Socket sending url(N) to python

---
+++ rltool/systems/comm.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+import time
+
+def main():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to the port
+    server_address = ('localhost', 8080)
+    print('starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+    connection, client_address = sock.accept()
+
+    connection.sendall("state".encode())
+    print(connection.recv(1000).decode())
+
+    connection.close()
+
+if __name__ == '__main__':
+    main()
