5ce4406597b1d7dd9a67c2cfb31391a9479f85f0
quadmium <Quadmium@gmail.com>
2020-10-27 23:26:04 -0500

    Buffered sockets, differentiate state fetch and url methods

--- rltool/systems/comm.py
+++ rltool/systems/comm.py
@@ -18,11 +18,10 @@ class Application:
 
         builder.connect_callbacks(self)
 
-        self.last_sent = None
-
         self.connect()
-        
+
     def run(self):
+        self.update_loop()
         self.mainwindow.mainloop()
 
     def connect(self):
@@ -39,49 +38,24 @@ class Application:
         sock.listen(1)
         self.connection, client_address = sock.accept()
 
+    
+    def update_loop(self):
+        self.fetch_state()
+        self.mainwindow.after(1000, self.update_loop)
+
+    def fetch_state(self):
+        self.connection.sendall(("FETCH-STATE#").encode())
+        self.set_url_text(self.connection.recv(1000).decode())
+
     def set_url_text(self, url):
         self.builder.get_variable("url_label_text").set("Current URL: {}".format(url))
 
     def on_button_send_click(self):
-        if self.builder.get_variable("url_entry_text").get() == self.last_sent:
-            return
-        self.last_sent = self.builder.get_variable("url_entry_text").get()
-        self.connection.sendall(self.last_sent.encode())
-        self.set_url_text(self.connection.recv(1000).decode())
+        new_url = self.builder.get_variable("url_entry_text").get()
+        self.connection.sendall(("MSG-NEW-URL|" + new_url + "#").encode())
+        self.fetch_state()
 
 
 if __name__ == '__main__':
     app = Application()
     app.run()
-
-'''
-import socket
-import sys
-import time
-
-def main():
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # Bind the socket to the port
-    server_address = ('localhost', 8080)
-    print('starting up on %s port %s' % server_address)
-    sock.bind(server_address)
-
-    # Listen for incoming connections
-    sock.listen(1)
-    connection, client_address = sock.accept()
-
-    i = 0
-    while True:
-        connection.sendall("url{}".format(i).encode())
-        i += 1
-        print(connection.recv(1000).decode())
-        time.sleep(5)
-
-    connection.close()
-
-if __name__ == '__main__':
-    main()
-'''
